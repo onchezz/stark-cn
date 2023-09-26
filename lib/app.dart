c99deee323f2a85a70f4084ccd5d1127a498c056
Onchez <56371447+onchezz@users.noreply.github.com>
2023-09-26 12:04:31 +0300

    upda

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,23 @@
 import 'package:starknet/starknet.dart';
 
 final provider = JsonRpcProvider.infuraGoerliTestnet;
-        
+
+// ignore_for_file: prefer_const_declarations
+
+import 'package:starknet/starknet.dart';
+
+final provider = JsonRpcProvider.infuraGoerliTestnet;
 final contractAddress = '0x076b4f19561a3c48f13aa3cb912ad92c0e702270466668ec9d77513ba6c5b0e2';
-Future<int> getCounter(String contractAddress) async {
+final secretAccountAddress = "0x00ce7b8175e1aed7e087f44e63051c053cda012d5f63fdd1e95e82489925ff41";
+final secretAccountPrivateKey = "0x06a1b5d41b7e5fee4310fda61d7c1b11e039f4681424cc89e1e8bfffe1ed9926";
+   final signeraccount = getAccount(
+  accountAddress:Felt.fromHexString(secretAccountAddress) ,
+  privateKey: Felt.fromHexString(secretAccountPrivateKey) ,
+  nodeUri: infuraGoerliTestnetUri,
+);
+
+
+Future<int> getCounter() async {
   final result = await provider.call(
     request: FunctionCall(
         contractAddress: Felt.fromHexString(contractAddress),
