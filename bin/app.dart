982113cdc71356856d5f952fdc443340b0f0fe60
Onchez <56371447+onchezz@users.noreply.github.com>
2023-09-26 12:05:16 +0300

    Update app.dart

--- bin/app.dart
+++ bin/app.dart
@@ -2,11 +2,11 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:app/app.dart' as app;
 
-void main(List<String> arguments) async {
+void main() async {
   
   final contractAddress = "0x076b4f19561a3c48f13aa3cb912ad92c0e702270466668ec9d77513ba6c5b0e2";
 
-  print('Counter: ${(await app.getCounter(contractAddress))}');
+  print('Counter: ${(await app.getCounter())}');
   print('Increasing counter');
   final ok = await app.increaseCounter(contractAddress);
   print('Transaction succeeded: $ok');
